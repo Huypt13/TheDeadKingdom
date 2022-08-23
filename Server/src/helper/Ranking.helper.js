c77d0d3f02f10263d980882f1a4bc382149b7b22
tulva <levananhtu.05012000@gmail.com>
2022-08-23 23:00:13 +0700

    feat: user service for getBalance, link wallet, reward after match and build smartcontract

--- Server/src/helper/Ranking.helper.js
+++ Server/src/helper/Ranking.helper.js
@@ -7,6 +7,14 @@ const getRank = (star) => {
   const rank = GameInfor.Ranking[Math.floor((star - 1) / 20)];
   return rank + " " + (star % 20 == 0 ? 1 : Math.floor(5 - (star % 20) / 5));
 };
+
+const getRankIndex = (star) => {
+  if (star > 100) {
+    return 6;
+  }
+  return Math.floor((star - 1) / 20) +1;
+};
+
 const getStar = (star) => {
   if (star <= 100) return star % 5 == 0 ? 5 : star % 5;
   return star % 100;
@@ -16,4 +24,4 @@ const getStar = (star) => {
 const getLevelRank = (star) => {
   return Math.floor(star / 30);
 };
-module.exports = { getRank, getStar, getLevelRank };
+module.exports = { getRank, getStar, getLevelRank, getRankIndex };

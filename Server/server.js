4d32b1f8f624bced2e960069c62396fda9153ddd
tuanbqhe <tuanbqhe141256@gmail.com>
2022-08-27 00:03:06 +0700

    fix loi vat

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 
 
@@ -108,5 +109,3 @@ app.use("/marketPlace", MarketPlaceRouter);
 Database.connect(app);
 server.listen(8080, "0.0.0.0");
 // server.listen(8080);
-const date = new Date();
-console.log("date", date.getHours(), date.getMinutes(), date.getSeconds);

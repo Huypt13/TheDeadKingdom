fadfdce8638467187705db053cf01100165793e6
tuanbqhe <tuanbqhe141256@gmail.com>
2022-08-26 14:38:35 +0700

    fix

--- Server/src/helper/GameInfor.helper.js
+++ Server/src/helper/GameInfor.helper.js
@@ -1,13 +1,13 @@
 module.exports = {
-  CountKillMaxPlayer: 1,
+  CountKillMaxPlayer: 4,
   CountKillMinPlayer: 1,
   CountKillTime: 300,
 
-  DestroyMaxPlayer: 1,
+  DestroyMaxPlayer: 4,
   DestroyMinPlayer: 1,
   DestroyTime: 600,
 
-  FlagMaxPlayer: 1,
+  FlagMaxPlayer: 4,
   FlagMinPlayer: 1,
   FlagTime: 600,
 
